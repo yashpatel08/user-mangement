3243a4d40df4e27eb0d88a7e0eb6b541e4768439
Yash <yashpatel54257@gmail.com>
2024-04-21 12:18:53 +0530

    Update db.js

--- config/db.js
+++ config/db.js
@@ -5,8 +5,6 @@ const connectDB = async () => {
         const connection = await mongoose.connect(process.env.MONGODB_URI, {
             useNewUrlParser: true,
             useUnifiedTopology: true,
-            useFindAndModify: false,
-            useCreateIndex: true
         });
         console.log(`MongoDB Connected: ${connection.connection.host}`);
     } catch (error) {
