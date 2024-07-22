3605638d0bcd9c15ebc62d076988cee0144e8199
Yash Patel <94280370+yashpatel08@users.noreply.github.com>
2024-07-22 20:42:53 +0530

    Update User.js

--- model/User.js
+++ model/User.js
@@ -5,7 +5,7 @@ import bcrypt from 'bcryptjs';
 const userSchema = new Schema({
     name: { type: String, required: true },
     age: { type: String, required: true },
-    email: { type: String, required: true },
+    email: { type: String, required: true,unique:true },
     password: { type: String, required: true },
 }, { timestamps: true });
 
